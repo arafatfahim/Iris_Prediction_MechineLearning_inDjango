c51632569cbee72d95a98cbb16850a25f09db8cc
Arafat Fahim <arafatfahim872@gmail.com>
2021-09-04 16:32:22 +0600

    Initial commit

---
+++ predict/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import pandas as pd
+from .models import PredResults
+# Create your views here.
+
+
+def predict(request):
+    return render(request, 'predict.html')
+
+
+def predict_chances(request):
+    
+    if request.POST.get('action') == 'post':
+        sepal_length = float(request.POST.get('sepal_length'))
+        sepal_width = float(request.POST.get('sepal_width'))
+        petal_length = float(request.POST.get('petal_length'))
+        petal_width = float(request.POST.get('petal_width'))
+
+        model = pd.read_pickle(r'C:\Users\arafa\Documents\GitHub\Irish_Prediction_MechineLearning_inDjango\new_model.pickle')
+
+        result = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+
+        classification = result[0]
+
+        PredResults.objects.create(sepal_length=sepal_length, sepal_width=sepal_width, petal_length=petal_length, petal_width=petal_width, classification=classification)
+        
+        return JsonResponse({'result': classification, 'sepal_length': sepal_length,
+                             'sepal_width': sepal_width, 'petal_length': petal_length, 'petal_width': petal_width},
+                            safe=False)
+
+def view_results(request):
+    # Submit prediction and show all
+    data = {"dataset": PredResults.objects.all()}
+    return render(request, "results.html", data)
