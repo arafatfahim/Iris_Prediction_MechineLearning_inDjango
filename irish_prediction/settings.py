a89560dcbdf37731778d10c40eb07b6c808f1791
Arafat Fahim <arafatfahim872@gmail.com>
2021-09-04 18:07:35 +0600

    4thcom

--- irish_prediction/settings.py
+++ irish_prediction/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'irish_prediction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-  #  'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-   # }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb1',
-        'USER': 'user1',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.postgresql',
+ #       'NAME': 'mydb1',
+ #       'USER': 'user1',
+  #      'PASSWORD': '12345',
+  #      'HOST': 'localhost',
+   #     'PORT': '5432',
+  #  }
+#}
 
 
 # Password validation
